6c47d7411e1ff8761eb69f5c71137f0c8dff77e3
Ignacio Zanella <125219012+ukino222@users.noreply.github.com>
2023-07-13 01:20:51 -0300

    Update funciones.js

--- webpersonal28/core/static/core/JS/funciones.js
+++ webpersonal28/core/static/core/JS/funciones.js
@@ -1,4 +1,4 @@
 
 function wapp() {
-    window.open('https://api.whatsapp.com/send?phone=54951111234&text=Hola%20Federico.%20Te%20contacto%20por%20las%20las%20clases%20de%20música%20');
+    window.open('https://api.whatsapp.com/send?phone=5491159295887&text=Hola%20Federico.%20Te%20contacto%20por%20las%20las%20clases%20de%20música%20');
   }  
